664e491d8d9a7d077f06bc468889ff0c78ea9c33
Salu Ramos <61796219+SaluRamos@users.noreply.github.com>
2026-01-25 04:11:17 -0300

    fix wait_login in IndeedBot

--- src/bots/indeed.py
+++ src/bots/indeed.py
@@ -14,7 +14,7 @@ class IndeedBot(Bot):
         self.driver.get("https://br.indeed.com/?from=gnav-homepage")
         while True:
             try:
-                elem = self.find_element(1, By.XPATH, "/html/body/div[1]/header/nav/div/div/div/div[2]/div[2]/div[2]/a")
+                elem = self.get_element(1, By.XPATH, "/html/body/div[1]/header/nav/div/div/div/div[2]/div[2]/div[2]/a")
                 break
             except Exception as e:
                 time.sleep(1)
